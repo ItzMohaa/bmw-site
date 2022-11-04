3ae343ab87c89c2927a7a467ee8708055517c7ab
Mohaa <itzmohaa@gmail.com>
2022-11-04 14:52:44 +0100

    hashrouter

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
 import { motion } from "framer-motion";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-    </BrowserRouter>
+    </HashRouter>
   
 );
 
