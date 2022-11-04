d7e1dac7657f44e0b295c32081301df7ebd35ae8
Mohaa <itzmohaa@gmail.com>
2022-11-04 14:44:42 +0100

    update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
 
             
                 <ul>
-                <NavLink to="/" end> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Home</motion.li></NavLink>
-                   <NavLink to="/history"> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>History</motion.li></NavLink>
-                   <NavLink to="/technology"> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Technology</motion.li></NavLink>
-                   <NavLink to= '/production'>  <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Production</motion.li></NavLink>
+                <NavLink to="/bmw-site" end> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Home</motion.li></NavLink>
+                   <NavLink to="/bmw-site/history"> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>History</motion.li></NavLink>
+                   <NavLink to="/bmw-site/technology"> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Technology</motion.li></NavLink>
+                   <NavLink to= '/bmw-site/production'>  <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Production</motion.li></NavLink>
                 </ul>
 
                 <button id = 'btn'>Contact Us</button>
