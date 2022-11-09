40ce6404328cbf8c4ac68779d2e849a0718a33ca
ItzMohaa <itzmohaa@gmaiil.com>
2022-11-09 14:49:30 +0100

    Update "removed home.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
-import Home from "./Components/Home";
+import Navbar from "./Components/Navbar";
+import { useLocation } from "react-router-dom";
+import { Route,Routes } from "react-router-dom";
+import Main from './Components/Main';
+import History from "./Components/History";
+import Technology from './Components/Technology';
+import { VariantContext } from "./Contexts/Variants";
+import { AnimatePresence } from "framer-motion";
+import Discover from "./Components/Discover";
+import { useState } from "react";
 
+const App = () => {
+    const variants = {
+        hidden:{
+            x:"-100vw",
+            opacity: 0
+        },
+        visible:{
+            x:0,
+            opacity:1
+        },
+        exit:{
+           x:"-100vw",
+           transition:{duration:1}
+           
+            
 
-function App() {
+        }
+    };
+    const location = useLocation();
+    const [bgcolor,  newcolor]= useState('black')
  
+   
+    return (
+       
 
-  return (
-    <div className="App">
-      
-     <Home/>
-    </div>
-  );
+        <div className="App" >
+
+            <VariantContext.Provider value={variants}>
+           
+            <Navbar />
+            
+
+            <AnimatePresence exitBeforeEnter>
+            <Routes location={location} key={location.key}>
+                <Route exact path="/" element={<Main/>}/>
+                <Route exact path="/history" element={<History/>}/>
+                <Route exact path="/technology" element={<Technology/>}/>
+                <Route excat path ='/discover' element={<Discover/>}/>
+                
+            </Routes>
+            </AnimatePresence>
+           
+            </VariantContext.Provider>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,11 +3,13 @@ import { NavLink } from "react-router-dom";
 import logo from './Images/logo.png'
 import { useContext } from "react";
 import { VariantContext } from "../Contexts/Variants";
+import { useState } from "react";
 
 const Navbar = () => {
     const variants = useContext(VariantContext);
+
     return ( 
-        <motion.div variants={variants} initial="hidden" animate="visible" transition={{type:"tween", duration:1}} className="navbar">
+        <motion.div variants={variants} initial="hidden" animate="visible" transition={{type:"tween", duration:1}} className="navbar ">
             
             <div className="logo">
                 <img src ={logo} alt='bmwlogo'/>
@@ -16,7 +18,7 @@ const Navbar = () => {
             
                 <ul>
                 <NavLink to="/" end> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Home</motion.li></NavLink>
-                   <NavLink to="/history"> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>History</motion.li></NavLink>
+                   <NavLink  to="/history"> <motion.li  whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>History</motion.li></NavLink>
                    <NavLink to="/technology"> <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Technology</motion.li></NavLink>
                    <NavLink to= '/production'>  <motion.li whileHover={{scale:1.4}} transition={{type:"spring", duration:0.8}}>Production</motion.li></NavLink>
                 </ul>
