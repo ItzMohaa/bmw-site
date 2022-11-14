24dbed4ca240f8b0cadedc80434d8d49be52bcb2
Mohaa <itzmohaa@gmail.com>
2022-11-14 10:05:22 +0100

    update mcolors

--- src/Components/Mcolors.jsx
+++ src/Components/Mcolors.jsx
@@ -13,8 +13,8 @@ const Mcolors = () => {
     return ( 
         <motion.div  className="mcolors">
             <motion.div variants={variants} initial={{height:0}} animate={{height:"100vh"}} transition={{ duration:1, delay:1}}  exit="exit" className="color1"></motion.div>
-            <motion.div variants={variants} initial={{height:0}} animate={{height:"100vh"}} transition={{ duration:1, delay:1}} exit="exit" className="color2"></motion.div>
-            <motion.div variants={variants} initial={{height:0}} animate={{height:"100vh"}} transition={{ duration:1, delay:1}}  exit="exit" className="color3"></motion.div>
+            <motion.div variants={variants} initial={{height:0}} animate={{height:"100vh"}} transition={{ duration:1, delay:1.1}} exit="exit" className="color2"></motion.div>
+            <motion.div variants={variants} initial={{height:0}} animate={{height:"100vh"}} transition={{ duration:1, delay:1.2}}  exit="exit" className="color3"></motion.div>
         </motion.div>
      );
 }
